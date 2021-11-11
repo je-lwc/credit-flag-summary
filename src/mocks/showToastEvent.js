52b54a1f9db27bb270e23fdcb8567c59bf406785
je-lwc <79720377+je-lwc@users.noreply.github.com>
2021-11-11 01:00:15 -0500

    commit changes

--- src/mocks/showToastEvent.js
+++ src/mocks/showToastEvent.js
@@ -1,6 +1,8 @@
-export default class extends CustomEvent {
+class ShowToastEvent extends CustomEvent {
   constructor({ title, message, variant }) {
     super();
     console.log('Show Toast', title, message, variant);
   }
 }
+
+export { ShowToastEvent };
