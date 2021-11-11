467e16af271a40f7295400fb885d25d0422b6957
je-lwc <79720377+je-lwc@users.noreply.github.com>
2021-11-11 00:19:50 -0500

    commit changes

--- src/apexMock/getAccountCreditFlag.js
+++ src/apexMock/getAccountCreditFlag.js
@@ -1,14 +1,15 @@
 import { getRandom } from './util';
 
 export default function () {
-  return new Promise((resolve) =>
+  return new Promise((resolve) => {
+    const f = window.creditFlags;
     setTimeout(() => {
       resolve({
-        CREDIT_RATE__c: creditRate,
-        CREDIT_RATE2__c: creditRate2,
-        CREDIT_RATE3__c: creditRate3,
-        CREDIT_RATE4__c: creditRate4
+        CREDIT_RATE__c: f.creditRate,
+        CREDIT_RATE2__c: f.creditRate2,
+        CREDIT_RATE3__c: f.creditRate3,
+        CREDIT_RATE4__c: f.creditRate4
       });
-    }, getRandom(1, 500))
-  );
+    }, getRandom(1, 500));
+  });
 }
